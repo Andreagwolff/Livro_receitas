58b8e3a92740e3d253ec7b00bca565d866007b64
Andre Wolff <andre.wolff@rede.ulbra.br>
2018-11-27 19:55:04 -0300

    Initial commit

---
+++ app/src/main/java/com/example/andre/livro_receitas/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.andre.livro_receitas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.MainActivity);
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/andre/livro_receitas/SalgadosActivity.java
@@ -0,0 +1,13 @@
+package com.example.andre.livro_receitas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SalgadosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_salgados6);
+    }
+}
